3cd2c601fe71967716e2bf455a32c0c073143699
Tony Won <tonywon89@gmail.com>
2015-07-13 21:33:47 -0400

    Made the reset button and its function

--- experiment-tic-tac-toe.js
+++ experiment-tic-tac-toe.js
@@ -2,10 +2,16 @@
 $(document).ready(function(){
   var player1Turn = $("#player1-checkbox"), player2Turn = $("#player2-checkbox");
   var player1Text = $("#player1-text"), player2Text = $("#player2-text");
+  var startButton = $('#start-button'), resetButton = $('#reset-button');
+
+  disable(resetButton);
+  $(".main-grid").hide();
 
   $("#start-button").click(function(){
-    $(this).prop("disabled",true);
+    disable(startButton);
+    enable(resetButton);
     $('.main-grid').fadeIn("slow");
+    $('.announcement').text("Go!")
     
     player1Turn.prop("checked", true);
     player1Text.addClass("active-turn");
@@ -23,16 +29,53 @@ $(document).ready(function(){
     
   });
 
+  resetButton.click(function(){
+    clearGrid();
+    enable(startButton);
+    disable(resetButton);
+    if (player1Turn.prop("checked")) {
+      removeTurn(player1Turn, player1Text);
+    } else {
+      removeTurn(player2Turn, player2Text);
+    }
+    $('.announcement').text("Ready to play another game? Press Start!")
+    $('.main-grid').fadeOut("slow");
+  });
+
+
+
+function disable(button) {
+  button.prop("disabled", true)
+}
+
+function enable(button) {
+  button.prop("disabled", false)
+}
+
 //This will change the turn from player 1 to player 2, or vice versa
 function changeTurn(currentTurn, currentText, nextTurn, nextText) {
-  currentTurn.prop("checked", false);
-  currentText.removeClass("active-turn");
-  nextTurn.prop("checked", true);
-  nextText.addClass("active-turn");
+  removeTurn(currentTurn, currentText);
+  addTurn(nextTurn,nextText);
+}
+
+function removeTurn(playerTurn, playerText) {
+  playerTurn.prop("checked",false)
+  playerText.removeClass("active-turn")
+}
+
+function addTurn(playerTurn, playerText) {
+  playerTurn.prop("checked", true);
+  playerText.addClass("active-turn");
 }
 
 //This will place either 0 or X for the entry, and place it in the corresponding box
 function placeMark(entry,location) {
   $(".entry", location).text(entry);
 }
+
+function clearGrid() {
+  $(".entry").empty();
+}
 });
+
+
